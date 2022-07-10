2866544125dc0bb7198b99122f52cea4259ab336
Mark_Lee <tlgud585@gmail.com>
2022-07-10 21:11:29 +0900

    [ADD] firebase SDK 추가 , 로그인 페이지 추가

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,12 @@
-function App() {
-    return <h1>Hi</h1>;
+import Login from "./components/pages/login/login/login";
+import styles from "./app.module.css";
+
+function App({ authService }) {
+    return (
+        <div className={styles.app}>
+            <Login authService={authService} />
+        </div>
+    );
 }
 
 export default App;
