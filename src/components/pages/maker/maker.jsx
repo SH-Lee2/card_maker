88b6dd87d5ac3a2f5b74daba1786b27773a8724d
Mark_Lee <tlgud585@gmail.com>
2022-07-11 22:44:58 +0900

    [ADD] editor , preview UI 추가

--- src/components/pages/maker/maker.jsx
+++ src/components/pages/maker/maker.jsx
@@ -8,6 +8,42 @@ import Editor from "./editor/editor";
 import styles from "./maker.module.css";
 import Preview from "./preview/preview";
 
+const CARDS = [
+    {
+        id: "1",
+        name: "SiHyeong",
+        company: "naver",
+        theme: "light",
+        title: "Software Engineer",
+        email: "tlgud585@gmail.com",
+        message: "go for it",
+        fileName: "sihyeong",
+        fileURL: null,
+    },
+    {
+        id: "2",
+        name: "SiHyeong2",
+        company: "naver",
+        theme: "dark",
+        title: "Software Engineer",
+        email: "tlgud585@gmail.com",
+        message: "go for it",
+        fileName: "sihyeong",
+        fileURL: null,
+    },
+    {
+        id: "3",
+        name: "SiHyeong3",
+        company: "naver",
+        theme: "colorful",
+        title: "Software Engineer",
+        email: "tlgud585@gmail.com",
+        message: "go for it",
+        fileName: "sihyeong",
+        fileURL: null,
+    },
+];
+
 function Maker({ authService }) {
     let navigate = useNavigate();
     const { state: id } = useLocation();
@@ -23,8 +59,8 @@ function Maker({ authService }) {
         <section className={styles.maker}>
             <Header onLogout={onLogout} />
             <div className={styles.container}>
-                <Editor />
-                <Preview />
+                <Editor cards={CARDS} />
+                <Preview cards={CARDS} />
             </div>
             <Footer />
         </section>
