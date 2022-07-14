0d9a6815478be303425dce3bba6ed6c518854345
Mark_Lee <tlgud585@gmail.com>
2022-07-15 01:00:29 +0900

    [ADD] 이미지 업로드 로딩 스피너 추가

--- src/UI/image_file_input/image_file_input.jsx
+++ src/UI/image_file_input/image_file_input.jsx
@@ -1,11 +1,15 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styles from "./image_file_input.module.css";
 
 const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
     const inputRef = useRef();
+    const [loading, setLoading] = useState(false);
+
     const uploadImage = async (event) => {
+        setLoading(true);
         const upload = await imageUploader //
             .upload(event.target.files[0]);
+        setLoading(false);
         onFileChange({
             fileName: upload.original_filename,
             fileURL: upload.url,
@@ -25,9 +29,17 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
                 accept="image/*"
                 onChange={uploadImage}
             />
-            <button className={styles.button} onClick={onClick}>
-                {name || "NO file"}
-            </button>
+            {!loading && (
+                <button
+                    className={`${styles.button} ${
+                        name ? styles.pink : styles.gray
+                    }`}
+                    onClick={onClick}
+                >
+                    {name || "NO file"}
+                </button>
+            )}
+            {loading && <div className={styles.loading}></div>}
         </div>
     );
 };
